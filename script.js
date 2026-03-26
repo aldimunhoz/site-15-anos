f2ea99c628c9cf3760f3e6938e1f4aee1c4f4efb
aldimunhoz <aldimunhoz@gmail.com>
2026-03-25 22:08:15 -0300

    animação + contador

---
+++ script.js
@@ -0,0 +1,13 @@
+const dataFesta = new Date("Oct 26, 2026 20:30:00").getTime();
+
+const x = setInterval(function () {
+  const agora = new Date().getTime();
+  const distancia = dataFesta - agora;
+
+  const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+
+  document.getElementById("countdown").innerHTML =
+    `<span>${dias} dias</span> | <span>${horas}h</span> | <span>${minutos}m</span>`;
+}, 1000);
